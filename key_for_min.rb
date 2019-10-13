38c95d75bdcb2a0d53f181c2777dc7eab208ac2c
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-13 18:34:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,6 @@ def key_for_min_value(name_hash)
   value_array = name_hash.collect {|key, value| value}
   i = value_array[0]
   value_array.each {|x| i = x if x < i}
-  name_hash.each {|key, value| key if value == i}
+  min_value = name_hash.each {|key, value| key if value == i}
+  min_value
 end
