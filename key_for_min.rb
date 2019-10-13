678165afd8c472257c2574d06a3ed63f13065adf
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-13 18:26:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  value_array = name_hash.collect {|key, value| value}
+  i = 0
+  while i < value_array.size do
+    
 end
